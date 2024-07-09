a3be6419aadc894eb31a21eb0a7177fbdaf8bb09
AEdwinGnanaOli <edwina3942@gmail.com>
2024-07-09 07:56:12 +0530

    comiited

--- server/Server.js
+++ server/Server.js
@@ -6,7 +6,7 @@ const cookieParser=require('cookie-parser')
 const app = express()
 const {MONGO_URL,PORT}=process.env
 app.use(cors({
-    origin: ['https://preeminent-kleicha-760af9.netlify.app'],
+    origin: ['https://beautiful-genie-4db327.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
