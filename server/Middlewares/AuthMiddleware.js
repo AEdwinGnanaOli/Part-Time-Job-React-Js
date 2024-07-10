d15d7125e2be85e476f36a92b4f42fb125a89fb7
AEdwinGnanaOli <edwina3942@gmail.com>
2024-07-10 15:05:55 +0530

    bb

--- server/Middlewares/AuthMiddleware.js
+++ server/Middlewares/AuthMiddleware.js
@@ -7,14 +7,21 @@ const likes=require('../Models/like')
 
 
 module.exports.userVerification=async(req,res)=>{
-          try{
-            const user = await UserModel.findById(data.id)
-            const vendorProducts= await ProductModel.find({})
-            if (user) {return res.json({ status: true,user,vendorProducts})}
-             else {return res.json({ status: false })}
-          }catch(err){
-            res.json({message:err.message})
-          }
+  const token=req.cookies.token
+  if(!token){
+      return res.json({ status: false })
+  }
+  jwt.verify(token,'jwt_secret_key',async(err,data)=>{
+      if (err) {
+          return res.json({ status: false })
+         } else {
+           const user = await UserModel.findById(data.id)
+           const vendorProducts= await ProductModel.find({})
+           if (user) {return res.json({ status: true,user,vendorProducts})}
+           else {return res.json({ status: false })}
+         }
+       })
+
 
              
         
